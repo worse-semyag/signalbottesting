fe38eec2724620b6865ab1ed1e21f394993a203f
semyag <jaymest.ross@gmail.com>
2026-02-13 12:56:53 -0500

    vehicleinfo functional for real

--- commands/platecheck.py
+++ commands/platecheck.py
@@ -40,7 +40,7 @@ class platecheck(Command):
                             print(sighting)
                             sightings_formatted = []
                             vehicle_info = None
-                            if "vehicle_id" in sighting[0]:
+                            if sighting[0].get("vehicle_id") is not None:
                                 print("VEHICLEID FOUND")
                                 vehicle_id = sighting[0]['vehicle_id']
                                 print("VEHICLE_ID" +vehicle_id)
@@ -52,25 +52,32 @@ class platecheck(Command):
                                     vehicle_info = vehicle_response.json()
                             else: 
                                 print("NO VEHICLEID")
+
                             #format sightings to look nice in signal
+                            print("STARTING SIGHTINGS LOOP")
                             for s in sighting:
                                 longitude = s["longitude"]
                                 latitude = s["latitude"]
                                 timestamp = s["timestamp"]
                                 plate= plate_code
-                                vehicle = s.get("vehicle_id", "unknown")
+                                #vehicle = s.get("vehicle_id", "unknown")
                                 
                                 line = (f"Location:{longitude},{latitude}",f" || Time:{timestamp}")
                                 sightings_formatted.append(line)
+                                print("LOOP")
                             #Format VEHICLE INFO
-                            vehicle_msg = (
+                            print("FORMATTING VEHICLE INFO")
+                            print(vehicle_info)
+                            if vehicle_info is not None: vehicle_msg = (
                                                 f"Make {vehicle_info.get('make', 'unknown')}\n"
                                                 f"Model  {vehicle_info.get('model', 'unknown')}\n"
                                                 f"Color  {vehicle_info.get('color', 'unknown')}"
                                             )
-
+                            else: vehicle_msg =("VEHICLE INFO UNKOWN")
+                            print(vehicle_msg)
                             msg = "\n\n".join(f"{loc},{time}" for loc,time in sightings_formatted)
                             #lets chat know one sighting reported
+                            print(f"MSG: {msg}")
                             if len(sighting) == 1:
                                 await c.send(f"One Sighting found\nPlate: {plate}\n{vehicle_msg}\n{msg}")
                             #lets chat know multiple sightings reported
@@ -81,7 +88,7 @@ class platecheck(Command):
                             await c.send(f"No Sightings found for plate {plate_code} please use /plateadd to add the plate")
                         
             else: 
-                    await c.reply("No Plate")
+                    await c.reply("No Plate Found")
                     print(response.status_code)
 
         except:
